947a924ab5bb65d22e6083ade12d5b2fa245008c
Adam Wright <adamtwright7@gmail.com>
2022-12-12 17:00:09 -0500

    Finished calc
    Calculator now has good adding functionality. It's finished for now.

--- daily-work/week4/day4-DOM/calculator/calculator.js
+++ daily-work/week4/day4-DOM/calculator/calculator.js
@@ -6,7 +6,7 @@ const clear = document.querySelector(".clear")
 const equal = document.querySelector(".equal")
 
 // Make a string that will hold each button press
-let userInput = []
+let userInput = ""
 
 // Add some event listeners to the numbers and operators. 
 // We just want to add these to the user input string, then display them.
@@ -21,8 +21,8 @@ for (let element of numbersAndOperators) {
 // It will just clear the result bar and the user input string.
 
 clear.addEventListener("click", () => {
-    userInput = []
-    resultBar.innerText = []
+    userInput = ""
+    resultBar.innerText = ""
 })
 
 // Now the hard part -- adding functionality to the equals button. Still using an event listener. 
@@ -31,17 +31,10 @@ clear.addEventListener("click", () => {
 let sum = 0 
 
 equal.addEventListener("click", () => {
-    // We're going to loop through the userInput string and do stuff with what we find. Tracks index. JavaScript for loops are trash
-    let inputIndex = 0
-    while (inputIndex < userInput.length){
-        if (userInput[inputIndex] == "+"){ // finds a plus
-            // Adds the numbers around a +
-            sum = userInput[inputIndex-1] + userInput[inputIndex+1]
-            // Replaces that part (around +) of userInput with the result
-            userInput.splice(inputIndex-1,3,sum) // splice isn't working for some reason 
-        } 
-        inputIndex ++ 
-    }
+    let toSum = userInput.split("+").map(Number) // splits the string at any + signs, turns those strings into numbers
+    sum = toSum.reduce((a, b) => a + b, 0) // then sums them all.
+    userInput = sum
+    resultBar.innerText = userInput
 })
 
 // putting the user input into a results bar that displays on the page
