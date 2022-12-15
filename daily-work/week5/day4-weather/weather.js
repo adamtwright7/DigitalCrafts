aadd5be47d791f4a16c9ee0c2aa162617bb42125
Adam Wright <adamtwright7@gmail.com>
2022-12-15 15:45:32 -0500

    deleted key
    deleted joe's key lolol

--- daily-work/week5/day4-weather/weather.js
+++ daily-work/week5/day4-weather/weather.js
@@ -3,7 +3,7 @@ const searchButton = document.querySelector(".search");
 const getWeather = async () => {
     const weatherInput = document.querySelector(".weather").value;
     const weatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${weatherInput}&appid=ff1cadec65d9f10c3aafc2a01b14612c`
+    `https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${weatherInput}&appid=`
     );
     const json = await weatherData.json();
 
