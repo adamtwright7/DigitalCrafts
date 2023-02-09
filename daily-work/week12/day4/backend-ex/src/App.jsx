c5588fa2701b5ff08e401746660e16393bf2ad92
Adam Wright <adamtwright7@gmail.com>
2023-02-09 14:29:15 -0500

    Form for day4 exercise
    Mostly complete...

---
+++ daily-work/week12/day4/backend-ex/src/App.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+function App() {
+  const [signUpForm, setSignUpForm] = useState({});
+
+  const setFormState = (e) => {
+    setSignUpForm({
+      ...signUpForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const authentication = () => {
+    if (signUpForm?.username?.length < 6 || signUpForm?.username?.length > 14) {
+      toast("Enter a username with 6-14 characters, inclusive.");
+    }
+    if (signUpForm?.zip?.length !== 5) {
+      toast("Enter a 5-digit zip code.");
+      console.log("incorrect zip");
+    }
+    if (
+      signUpForm?.ageGroup?.length !== 3 ||
+      signUpForm.ageGroup.slice(-1) !== "s"
+    ) {
+      toast("Enter your age group, like so: 30s");
+    }
+  };
+
+  return (
+    <div class="bg-[#D44D78] h-screen">
+      <div class="mx-auto pt-10 w-[50vw] bg-[#1D2355] shadow-xl hover:shadow-2xl">
+        <div className="mx-auto max-w-md">
+          <p class="text-white text-center font-semibold text-2xl">
+            CONTACT FORM
+          </p>
+          <form className="shadow-md rounded p-8 my-4">
+            <div className="mb-4 flex">
+              <img src="../public/profile.jpg" />
+              <input
+                className="border-b-slate-600 border-b-2 bg-inherit w-full p-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Enter Your Username"
+                onChange={(e) => {
+                  setFormState(e);
+                }}
+                value={signUpForm.username ? signUpForm.username : ""}
+              />
+            </div>
+            <div className="mb-6 flex">
+              <img src="../public/mail.jpg" />
+              <input
+                className="border-b-slate-600 border-b-2 bg-inherit w-full p-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="zip"
+                type="zip"
+                name="zip"
+                placeholder="Enter your 5-digit zip code, like so: 30000"
+                onChange={(e) => {
+                  setFormState(e);
+                }}
+                value={signUpForm.zip ? signUpForm.zip : ""}
+              />
+            </div>
+            <div className="mb-6 flex">
+              <img src="../public/profile.jpg" />
+              <input
+                className="border-b-slate-600 border-b-2 bg-inherit w-full p-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="ageGroup"
+                name="ageGroup"
+                type="ageGroup"
+                placeholder="Enter your age group, like so: 30s"
+                onChange={(e) => {
+                  setFormState(e);
+                }}
+                value={signUpForm.ageGroup ? signUpForm.ageGroup : ""}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-[#D44D78] text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline mx-auto p-4"
+                type="button"
+                onClick={authentication()}
+              >
+                SUBMIT
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ daily-work/week12/day4/tailwind-react/src/components/SignUp.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+const SignUp = () => {
+  const [signUpForm, setSignUpForm] = useState({});
+
+  const setFormState = (e) => {
+    setSignUpForm({
+      ...signUpForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={(e) => {
+                setFormState(e);
+              }}
+              value={signUpForm.username ? signUpForm.username : ""}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="adam@gmail.com"
+              onChange={(e) => {
+                setFormState(e);
+              }}
+              value={signUpForm.email ? signUpForm.email : ""}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="******************"
+              onChange={(e) => {
+                setFormState(e);
+              }}
+              value={signUpForm.password ? signUpForm.password : ""}
+            />
+            <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
